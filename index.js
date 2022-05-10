ba6233e87308b2500f6b285907a07cd8a4b50086
Briana McCloud <bmccloud26122@gmail.com>
2022-05-10 00:11:45 -0400

    commit tree 66cae7d8023a18f672c93eeff1af9284cd79c88a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.getElementById('main').remove();
+// const newHeader = document.createElement('h1')
+   console.log("test")
+   const newHeader = document.createElement('h1')
+   newHeader.id = 'victory'
+   newHeader.innerHTML = "Briana is the champion"
